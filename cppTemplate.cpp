53837933cfe4adba0ca8dd1b08f29dd0688422a3
Jeethendra S R <jeethendrajeethu8@gmail.com>
2022-04-03 02:16:42 -0700

    Added gitignore and basic cpp template

---
+++ cppTemplate.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define newLine cout << "\n"
+#define println(a) cout << (a) << "\n"
+#define testCases int t; cin >> t; while(t--)
+#define debug(a) cout << #a << " = " << a << "\n"
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define iofiles freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout)
+
+const ll MOD = 1e9 + 7;
+
+
+int main(){
+    fastIO;
+    
+    return 0;
+}
